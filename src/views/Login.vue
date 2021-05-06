dc1ee5a123fb6a69d74bdd0f88e65aaed3aca2cb
Rodion Sibov <rodionsibov@gmail.com>
2021-05-07 02:25:29 +0300

    first commit

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <input v-model="email" type="text" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <input type="submit" value="Login" />
+      <p>
+        Need an account?
+        <router-link to="/register">Register Here</router-link>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import firebase from "firebase";
+
+export default {
+  setup() {
+    const email = ref("test@test.com");
+    const password = ref("123456");
+
+    const login = () => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value)
+        .then((data) => console.log(data))
+        .catch((err) => alert(err.message));
+    };
+
+    return {
+        email,
+        password,
+        login
+    }
+  },
+};
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <form @submit.prevent="register">
+      <input v-model="email" type="text" placeholder="Email" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <input type="submit" value="Register" />
+      <p>
+        Have an account?
+        <router-link to="/login">Login Here</router-link>
+      </p>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+
+    const register = () => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email.value, password.value)
+        .then((user) => {
+          alert(user);
+        })
+        .catch((err) => alert(err.message));
+    };
+
+    return {
+        email,
+        password,
+        register
+    }
+  },
+};
+</script>
